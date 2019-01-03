c57189140fd75b3f681f8167b423d9d9c1bd3c3d
Thomas Pope <tpope0928@gmail.com>
2019-01-03 21:19:03 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -91,5 +91,8 @@ class Student
   def self.first_X_students_in_grade_10
   end
   
-  def self
+  def self.first_student_in_grade_10 returns the first student in grade 10
+  end
+  
+  def self.
 end
