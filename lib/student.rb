1edea4508532e6b7d016bd6963e8cf7d50898144
Thomas Pope <tpope0928@gmail.com>
2019-01-03 21:22:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -90,10 +90,15 @@ class Student
   
   def self.first_X_students_in_grade_10
     
-    
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
   
   def self.first_student_in_grade_10 returns the first student in grade 10
+    
+    
+    
   end
   
   def self..all_students_in_grade_X returns an array of all students in a given grade X
