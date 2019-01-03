17aa0c22c4b835477a04686fdba2a95c4b20ae85
Thomas Pope <tpope0928@gmail.com>
2019-01-03 21:29:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -102,5 +102,14 @@ class Student
     end
   end
   
-  def self.
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10 
+      ORDER BY students.id
+      LIMIT 1
+    SQL
+    
+    
 end
