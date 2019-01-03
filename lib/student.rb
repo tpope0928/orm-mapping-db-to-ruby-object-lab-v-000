bfa6352b463f0db01daa3be3a50ec0931d35d70e
Thomas Pope <tpope0928@gmail.com>
2019-01-03 21:25:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -93,7 +93,9 @@ class Student
       SELECT *
       FROM students
       WHERE grade = 10
-      
+      ORDER BY student.id 
+      LIMIT 1
+    SQL
     
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
